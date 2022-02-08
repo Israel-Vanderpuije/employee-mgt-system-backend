9bc7e04f1ed7f17f686cfa3cbea3c802e825944c
Israel Vanderpuije <vanderpuijeisrael7@yahoo.com>
2022-02-08 10:41:59 +0000

    added controller, exception and repository files

--- src/main/java/com/izzy/employeemanager/entity/Employee.java
+++ src/main/java/com/izzy/employeemanager/entity/Employee.java
@@ -1,4 +1,32 @@
 package com.izzy.employeemanager.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@AllArgsConstructor
+@Table(name = "employee")
 public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "employee_id")
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    private String email;
+
+    public Employee(String firstName, String lastName, String email){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
 }
---
+++ src/main/java/com/izzy/employeemanager/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.izzy.employeemanager.repository;
+
+import com.izzy.employeemanager.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
